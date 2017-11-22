42ba96a2942196fe322514db0981d0a5fb7e5031
Nazar173 <Nazar.Gonor.123@gmail.com>
2017-11-22 11:38:49 +0200

    day3

--- day4/6.cpp
+++ day4/6.cpp
@@ -1,5 +1,4 @@
-//Naiiti primer figyri ABCD po zadanim storonam
-//AB, AC i DC
+/* Naiti pirimiter figuri ABCD po zadanim storonam AB,AC,DC.*/
 
 
 
@@ -8,22 +7,34 @@
 #include <cmath>
 using namespace std;
  
-double figura(double x, double y)
+double func_Pifagora(double x, double y)
 {
     return sqrt(x*x + y*y);
 }
  
+ double func_Heroin(double x, double y, double h)
+ 
+{double PP, p;
+PP=(x+y+h)/2;
+    return sqrt(PP*(PP-x)*(PP-y)*(PP-h));;
+}  
+
+
 int main()
 {
-    const double AB = 5;
-    const double AC = 6;
-    const double CD = 5;
+     double AB,AC,CD,BD ;
+  cout << "Vvedit AB= "<< endl ;
+  cin>>AB;  
+    cout << "Vvedit AC= " << endl ;
+    cin>>AC; 
+      cout << "Vvedit CD= " << endl;  
+      cin>>CD;
+        cout << "Vvedit BD= " <<  endl;
+        cin>>BD;
  
-    double BC = figura (AB, AC);
-    double BD = figura (BC, CD);
- 
-    cout << "The figura thrikytnik "
-              << AB + AC + CD + BD;
+    cout << "Vaha plosha=" <<func_Heroin(AB,AC,func_Pifagora(AB,AC))+func_Heroin(BD,CD,func_Pifagora(AB,AC))<<endl;
+   
  system("pause");
     return 0;
 }
+
